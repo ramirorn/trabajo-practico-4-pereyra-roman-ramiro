54192f4e9d724fe44bc78a73a57624ad6b55bc81
Ramiro <ramiroroman306@gmail.com>
2025-06-23 11:24:34 -0300

    Se creo el modelo de Character

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
+// Importaciones
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import { startDb } from "./src/config/database.js";
 
+// Definicion de variables
 const app = express();
 const PORT = process.env.PORT;
 
+// Conexion con la base de datos
 startDb();
+
+// Inicio del servidor
 app.listen(PORT, () => {
     console.log(`SERVIDOR CORRIENDO EN: http://localhost:${PORT}`)
 });
